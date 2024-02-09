efa46a6264eba625d91a74544e5e8d15a7902105
Joytunnahar <joytunnahar@gmail.com>
2024-02-09 09:32:05 +0600

    404 page added

--- searchform.php
+++ searchform.php
@@ -8,7 +8,7 @@
 <form action="<?php echo home_url('/'); ?>" method="get">
         <div class="search-block" style="display: none;">
           <label for="search-field" class="w-100 mb-0">
-            <input type="text" class="form-control" name="s" value="<?php echo get_search_query(' '); ?>" id="search-field" placeholder="Type what you want and enter">
+            <input type="text" class="form-control" name="s" value="<?php echo get_search_query(); ?>" id="search-field" placeholder="Type what you want and enter">
           </label>
           <span class="search-close">&times;</span>
         </div><!-- Site search end -->
